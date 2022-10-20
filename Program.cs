ed6d6bbcbd365d491eb86412bc34d17dd87c438c
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2022-10-20 23:27:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UserRegistration;
+
+Console.WriteLine("Welcome to User Registration Problem");
+Console.WriteLine("Enter 1 for First Name");
+Console.WriteLine("Enter 2 for Last Name");
+Console.WriteLine("Enter 3 for Email");
+Console.WriteLine("Enter 4 for Mobile number");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch (num)
+{
+    case 1:
+        RegexFirstName firstName = new RegexFirstName();
+        bool ValidName = firstName.validateString("Arundhati");
+        Console.WriteLine("{0}", (ValidName) ? "Valid" : "Invalid");
+        break;
+
+    case 2:
+        RegexLastName lastName = new RegexLastName();
+        bool ValidName2 = lastName.validateString("Raina");
+        Console.WriteLine("{0}", (ValidName2) ? "Valid" : "Invalid");
+        break;
+
+    case 3:
+        RegexEmail Email = new RegexEmail();
+        bool Email1 = Email.validateString("rainaarundhati16@gmail.com");
+        Console.WriteLine("{0}", (Email1) ? "Valid" : "Invalid");
+        break;
+
+    case 4:
+        RegexMobileNumber regexMobileNumber = new RegexMobileNumber();
+        bool Number1 = regexMobileNumber.validateString("91 9769310857");
+        Console.WriteLine("{0}", (Number1) ? "Valid" : "Invalid");
+        break;
+}
