a05799949ae3178e90f810d0d7d0028c1a854d6f
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2022-10-20 23:11:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UserRegistration;
+
+Console.WriteLine("Welcome to User Registration Problem");
+Console.WriteLine("Enter 1 for First Name");
+Console.WriteLine("Enter 2 for Last Name");
+Console.WriteLine("Enter 3 for Email");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch (num)
+{
+    case 1:
+        RegexFirstName firstName = new RegexFirstName();
+        bool ValidName = firstName.validateString("Ranju");
+        Console.WriteLine("{0}", (ValidName) ? "Valid" : "Invalid");
+        break;
+
+    case 2:
+        RegexLastName lastName = new RegexLastName();
+        bool ValidName2 = lastName.validateString("Rajappa");
+        Console.WriteLine("{0}", (ValidName2) ? "Valid" : "Invalid");
+        break;
+
+    case 3:
+        RegexEmail Email = new RegexEmail();
+        bool Email1 = Email.validateString("abc.xyz@bl.co.in");
+        Console.WriteLine("{0}", (Email1) ? "Valid" : "Invalid");
+        break;
+}
