fe359bbafd254ca9dbb8ab5fa43bd20f63cbc4fa
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2022-10-20 23:02:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UserRegistration;
+
+Console.WriteLine("Welcome to User Registration Problem");
+Console.WriteLine("Enter 1 for First Name");
+Console.WriteLine("Enter 2 for Last Name");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch (num)
+{
+    case 1:
+        RegexFirstName firstName = new RegexFirstName();
+        bool ValidName = firstName.validateString("Arundhati");
+        Console.WriteLine("{0}", (ValidName) ? "Valid" : "Invalid");
+        break;
+
+    case 2:
+        RegexLastName lastName = new RegexLastName();
+        bool ValidName2 = lastName.validateString("Raina");
+        Console.WriteLine("{0}", (ValidName2) ? "Valid" : "Invalid");
+        break;
+}
