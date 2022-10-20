b913f3cdfa2a3f736fa26b32172156ea506dcd11
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2022-10-20 23:06:10 +0530

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,16 @@
+using UserRegistration;
+
+Console.WriteLine("Welcome to User Registration Problem");
+
+RegexFirstName firstName = new RegexFirstName();
+
+if (firstName.validateString("Ranjitha"))
+
+{
+    Console.WriteLine("The Name is Valid");
+
+}
+else
+{
+    Console.WriteLine("The Name is InValid");
+}
